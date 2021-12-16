349fe77a49f9620ea0ba1ef003b37120cc722ebe
Eddington Chamunorwa <dittonedo45@github.com>
2021-12-16 15:21:14 +0200

    Incantion with jansson library plus partial argument handling.

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <archive.h>
 #include <archive_entry.h>
 #include <libavformat/avio.h>
+#include <getopt.h>
 
 char buf[10540];
 
@@ -20,51 +21,126 @@ static int dn_cst_reader(struct archive *a, void *d, const void **b)
     return ret == AVERROR_EOF ? 0 : ret;
 }
 
+#include <jansson.h>
+
 int main(signed Argsc, char *(Args[]))
 {
+    struct option lopts[] = {
+	{ "help", 0, 0, 'h' },
+	{ "option", 1, 0, 'o' },
+	{ "url", 1, 0, 'u' },
+	{ "path", 1, 0, 'p' },
+	{ "verbose", 0, 'v' },
+	{.name = 0 }
+    };
 
-    struct archive *a = archive_read_new();
-    struct archive_entry *e = archive_entry_new();
-    AVIOContext *ioc = 0;
+    if (argsc == 1) {
+	dndw_help(lopts, Args, 0);
+	exit(1);
+    }
+    json_t *arch = json_object();
 
-    int ret = 0;
+    json_t *arr = json_array();
+    json_t *url = json_string("");
+    json_t *path = json_string(getcwd(0, 0));
+    json_t *verbose = json_boolean(0);
+    json_t *nh = json_boolean(0);
 
-    ret = avio_open(&ioc, Args[1], AVIO_FLAG_READ);
+    struct {
+	const char *name;
+	json_t *obj;
+    } *p, mp[] = {
+	{ "options", arr },
+	{ "url", url },
+	{ "path", path },
+	{ "verbose", verbose },
+	{.name = NULL }
+    };
+    p = mp;
+
+    while (p && p->name) {
+	json_object_set(arch, p->name, p->obj);
+	p++;
+    }
 
     do {
-	if (ret < 0)
+	int c = getopt_long(Argsc, Args, "ho:u:p:v", lopts, 0);
+	if (c == -1)
 	    break;
-	archive_read_set_callback_data(a, ioc);
-	archive_read_set_read_callback(a, dn_cst_reader);
-	archive_read_set_seek_callback(a, 0);
-
-	archive_read_support_format_all(a);
-	archive_read_support_filter_all(a);
-
-	ret = archive_read_open1(a);
-
-	if (ret != ARCHIVE_OK)
+	switch (c) {
+	case 'o':
+	    json_t * o = dndw_parse_opt(optarg);
+	    if (o)
+		json_array_append(arr, o);
 	    break;
-	while (1) {
-	    ret = archive_read_next_header(a, &e);
-	    if (ret == ARCHIVE_EOF)
+	case 'u':
+	    json_string_set(url, optarg);
+	    break;
+	case 'p':
+	    json_string_set(path, optarg);
+	    break;
+	case 'v':
+	    json_object_set(arch, "verbose", json_boolean(1));
+	    break;
+	};
+    } while (1);
+    if (json_boolean_value(nh)) {
+	dndw_parse_opt(Argsc, Args, 0);
+	return 3;
+    }
+    if (!strlen(json_string_value(url)))
+	return 2;
+
+    do {
+	const char *url = json_string_value(url);
+	const char *path = json_string_value(path);
+
+	if (strlen(path)) {
+	    if (chdir(path))
 		break;
-	    else if (ret == ARCHIVE_RETRY)
-		continue;
-	    else if (ret == ARCHIVE_OK) {
-		puts(archive_entry_pathname(e));
-		archive_read_extract(a, e, 0);
-	    }
 	}
 
-      end:
-	;;
+	struct archive *a = archive_read_new();
+	struct archive_entry *e = archive_entry_new();
+	AVIOContext *ioc = 0;
+
+	int ret = 0;
+
+	ret = avio_open(&ioc, url, AVIO_FLAG_READ);
+
+	do {
+	    if (ret < 0)
+		break;
+	    archive_read_set_callback_data(a, ioc);
+	    archive_read_set_read_callback(a, dn_cst_reader);
+	    archive_read_set_seek_callback(a, 0);
+
+	    archive_read_support_format_all(a);
+	    archive_read_support_filter_all(a);
+
+	    ret = archive_read_open1(a);
+
+	    if (ret != ARCHIVE_OK)
+		break;
+	    while (1) {
+		ret = archive_read_next_header(a, &e);
+		if (ret == ARCHIVE_EOF)
+		    break;
+		else if (ret == ARCHIVE_RETRY)
+		    continue;
+		else if (ret == ARCHIVE_OK) {
+		    puts(archive_entry_pathname(e));
+		    archive_read_extract(a, e, 0);
+		}
+	    }
+
+	} while (0);
+
+	avio_close(ioc);
+
+	archive_read_close(a);
+	archive_entry_free(e);
     } while (0);
-
-    avio_close(ioc);
-
-    archive_read_close(a);
-    archive_entry_free(e);
 
     return (ret == AVERROR_EOF) == 0;
 }
